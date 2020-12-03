ea32617e2b1e2554b5265832dadb4f555378a456
Vitor Silva <vitor.lourenco@opencircle.com.br>
2020-12-03 17:37:26 -0300

    create setting up tooling

--- library/app.js
+++ library/app.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/jquery/dist"))
+);
+app.set("views", path.join(__dirname, "src", "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello from my library app");
+  res.render("index", { list: ["a", "b"], title: "Library" });
 });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
+  console.log(`listening on port ${chalk.green(port)}`);
 });
